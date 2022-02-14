5046c18b0805f346b762696aefca6c08f7125046
Ruben De Smet <user.name>
2022-02-14 16:05:49 +0100

    les 1-5

---
+++ 05-meeting-the-primitive-values.js
@@ -0,0 +1,27 @@
+console.log(typeof(undefined));
+let person = undefined;
+//console.log(person.mood);
+let bandersnatch;
+console.log(bandersnatch);
+//console.log(jabberwocky);
+let jabberwocky;
+let mimsy = null;
+//console.log(mimsy.mood);
+console.log(typeof(null));
+console.log(typeof(true));
+console.log(typeof(false));
+let isSad = true;
+let isHappy = !isSad; 
+let isFeeling = isSad || isHappy; 
+let isConfusing = isSad && isHappy;
+console.log(typeof(28)); // "number"
+console.log(typeof(3.14)); // "number"
+console.log(typeof(-140));
+console.log(0.1 + 0.2 === 0.3);
+console.log(0.1 + 0.2 === 0.30000000000000004);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER + 1);
+console.log(Number.MAX_SAFE_INTEGER + 2);
+console.log(Number.MAX_SAFE_INTEGER + 3);
+console.log(Number.MAX_SAFE_INTEGER + 4);
+console.log(Number.MAX_SAFE_INTEGER + 5);
