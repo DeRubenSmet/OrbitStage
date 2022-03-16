f2a440cdf9897568c6931cd60a2f5025e252d2dd
Ruben De Smet <user.name>
2022-03-16 09:37:17 +0100

    excel

--- kaart_oefening_excel/src/App.js
+++ kaart_oefening_excel/src/App.js
@@ -10,10 +10,11 @@ import geojsonProvinciesVlaanderen from './Provincies.json';
 import geojsonArrondissementenVlaanderen from './ArrondissementenVlaanderen.json';
 // import {dataLayer} from './map-style';
 import ControlPanel from './control-panel';
+import { Legend } from './components/Legend.js';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmFyYmFyb3NzbyIsImEiOiJja3ptd2Zlb3AwMDIyMm9xb3B3bjhqYjJiIn0.R0SXEE12p1SX1UbF7wqZ7g';
 
-const legendItems = [
+const legendItemsGemeentes = [
   {
       label: "Niet geweten",
       bgColor: "#3C3A38",
@@ -22,19 +23,104 @@ const legendItems = [
       label: "Undefined",
       bgColor: "#A0A0A0",
   },
-  // <div><div style={{ backgroundColor: '#3C3A38' }}></div>Niet geweten</div>
-  // <div><div style={{ backgroundColor: '#A0A0A0' }}></div>Undefined</div>
-  // <div><div style={{ backgroundColor: '#8B4225' }}></div>700000</div>
-  // <div><div style={{ backgroundColor: '#A25626' }}></div>600000</div>
-  // <div><div style={{ backgroundColor: '#B86B25' }}></div>500000</div>
-  // <div><div style={{ backgroundColor: '#CA8323' }}></div>400000</div>
-  // <div><div style={{ backgroundColor: '#DA9C20' }}></div>300000</div>
-  // <div><div style={{ backgroundColor: '#E6B71E' }}></div>200000</div>
-  // <div><div style={{ backgroundColor: '#EED322' }}></div>100000</div>
-  // <div><div style={{ backgroundColor: '#F2F12D' }}></div>0</div>
+  {
+    label: "700000",
+    bgColor: "#8B4225",
+  },
+  {
+    label: "600000",
+    bgColor: "#A25626",
+  },  
+  {
+    label: "500000",
+    bgColor: "#B86B25",
+  },
+  {
+    label: "400000",
+    bgColor: "#CA8323",
+  },
+  {
+    label: "300000",
+    bgColor: "#DA9C20",
+  },
+  {
+    label: "200000",
+    bgColor: "#E6B71E",
+  },
+  {
+    label: "100000",
+    bgColor: "#EED322",
+  },
+  {
+    label: "0",
+    bgColor: "#F2F12D",
+  },
+]
+
+const legendItemsArrondissementen = [
+  {
+      label: "Niet geweten",
+      bgColor: "#3C3A38",
+  },
+  {
+      label: "Undefined",
+      bgColor: "#A0A0A0",
+  },
+  {
+    label: "241",
+    bgColor: "#CA8323",
+  },
+  {
+    label: "214",
+    bgColor: "#DA9C20",
+  },
+  {
+    label: "184.99",
+    bgColor: "#E6B71E",
+  },
+  {
+    label: "183",
+    bgColor: "#EED322",
+  },
+  {
+    label: "179",
+    bgColor: "#F2F12D",
+  },
+]
+
+const legendItemsProvincies = [
+  {
+      label: "Niet geweten",
+      bgColor: "#3C3A38",
+  },
+  {
+      label: "Undefined",
+      bgColor: "#A0A0A0",
+  },
+  {
+    label: "241",
+    bgColor: "#CA8323",
+  },
+  {
+    label: "214",
+    bgColor: "#DA9C20",
+  },
+  {
+    label: "184.99",
+    bgColor: "#E6B71E",
+  },
+  {
+    label: "183",
+    bgColor: "#EED322",
+  },
+  {
+    label: "179",
+    bgColor: "#F2F12D",
+  },
 ]
 
 function App() {
+  const [keyData, setKeyData] = useState(Math.random());
   const mapRef = React.useRef();
   const [jsonFromCsv, setJsonFromCsv] = useState('');
   const [Gemeente, setGemeente] = useState({});
@@ -45,14 +131,15 @@ function App() {
   const [fillOpacityProvincies, setFillOpacityProvincies] = useState(0.75);
   const [loaded, setLoaded] = useState(false);
   const [displayLegend, setDisplayLegend] = useState('none');
-  const [year, setYear] = useState(2015);
+  const [year, setYear] = useState(2010);
   const [allData, setAllData] = useState(null);
+  const [legendItemsLayer, setLegendItemsLayer] = useState(legendItemsProvincies);
+  const [stateGeojsonArrondissementenVlaanderen, setStateGeojsonArrondissementenVlaanderen] = useState([]);
   //const [jsonOverheid, setJsonOverheid] = useState('');
   const [viewport, setViewport] = useState({
     latitude: 50.616949,
     longitude: 4.885260,
     zoom: 6,
-
   });
   const options = {
     delimiter: ';', header: true, dynamicTyping: true, complete: function (results, file) {
@@ -100,25 +187,33 @@ function App() {
     return municipality;
   }
 
+  
   // const zoekGemeente2 = () => {
   //     const result = jsonFromCsv.data.filter(element => element[2] === '2010' && element[4] > 100);
   //     setFilteredMunicipalities(result);
   // }
 
   const [coordinatesJsonVlaanderen, setCoordinatesJsonVlaanderen] = useState([]);
+  const [testCsv, setTestCsv] = useState([]);
+  const [testCsvProvincies, setTestCsvProvincies] = useState([]);
+  const [testCsvArrondissementen, setTestCsvArrondissementen] = useState([]);
+  useEffect(() => {
+    (async() => {
+      const csv = await axios.get("http://localhost:3000/NL_immo_jaar_v2.csv");
+      const csvProvincies = await axios.get("http://localhost:3000/csvProvincies.csv");
+      const csvArrondissementen = await axios.get("http://localhost:3000/csvArrondissementenVlaanderen.csv");
+      //setJsonFromCsv(papa.parse(csv.data, options));
+      setTestCsv(papa.parse(csv.data, options));
+      setTestCsvProvincies(papa.parse(csvProvincies.data, options));
+      setTestCsvArrondissementen(papa.parse(csvArrondissementen.data, options));
+    })()
+  }, [])
 
-  useEffect(async function getJsonFromCsv() {
+  useEffect(function getJsonFromCsv() {
+    
     //const jsonOverheid = await axios.get("https://raw.githubusercontent.com/Datafable/rolling-blackout-belgium/master/data/geospatial/municipalities-belgium.geojson");
-    const csv = await axios.get("http://localhost:3000/NL_immo_jaar_v2.csv");
-    const csvProvincies = await axios.get("http://localhost:3000/csvProvincies.csv");
-    const csvArrondissementen = await axios.get("http://localhost:3000/csvArrondissementenVlaanderen.csv");
-    //setJsonFromCsv(papa.parse(csv.data, options));
-    const testCsv = papa.parse(csv.data, options);
-    const testCsvProvincies = papa.parse(csvProvincies.data, options);
-    const testCsvArrondissementen = papa.parse(csvArrondissementen.data, options);
-    console.log(testCsvArrondissementen);
-    var flat = [];
-
+    
+    
     // function flatten(nested) {
 
     //   for (var i = 0; i < nested.length; i++) {
@@ -162,37 +257,49 @@ function App() {
     // const test = lambert72toWGS84(212426.26600000262, 204485.9840000011);
     // console.log(test);
     //setGeojson({...geojson.features[0], ...jsonFromCsv})
+
     for (let i = 0; i < geojsonGemeentesVlaanderen.features.length; i++) {
       
       const municipalityOverheid = geojsonGemeentesVlaanderen.features[i].properties.NAAM.split('#')[0].toUpperCase();
       
-      let ObjectMunicipalityCsv = '';
 
       // console.log(municipalityOverheid.split('#')[0].toUpperCase());
-      ObjectMunicipalityCsv = testCsv.data.find(element => element.municipality === municipalityOverheid);
+      const ObjectMunicipalityCsv = testCsv.data.filter(element => element.municipality === municipalityOverheid && element.year === year);
       if (ObjectMunicipalityCsv === undefined) {
         geojsonGemeentesVlaanderen.features[i].properties.mediaan1 = 10000000000000;
       }
       else {
-        geojsonGemeentesVlaanderen.features[i].properties.mediaan1 = ObjectMunicipalityCsv.mediaan1;
+        console.log(ObjectMunicipalityCsv);
+        geojsonGemeentesVlaanderen.features[i].properties.mediaan1 = ObjectMunicipalityCsv[0].mediaan1;
       }
     }
-    for(const province of geojsonProvinciesVlaanderen.features){
+    // const activeProvinceYears = testCsvProvincies.data.filter((item) => item.jaar === Number(year));
+    // for(const province of geojsonProvinciesVlaanderen.features){
 
-      const ObjectProvinceCsv = testCsvProvincies.data.find(element => element?.provincie?.split?.(' ')[1].toUpperCase() === province.properties.NAAM.toUpperCase());
+    //   const ObjectProvinceCsv = activeProvinceYears.find(element => element?.provincie?.split?.(' ')[1].toUpperCase() === province.properties.NAAM.toUpperCase());
       
-      province.properties.mediaan1 = ObjectProvinceCsv.mediaan1;
-    }
+    //   province.properties.mediaan1 = Number(ObjectProvinceCsv.mediaan1);
+    // }
+    console.log(testCsvArrondissementen);
+    const activeYears = testCsvArrondissementen.data.filter((item) => item.jaar === Number(year));
     for(const arrondissement of geojsonArrondissementenVlaanderen.features){
       // console.log(testCsvArrondissementen);
-      const ObjectArrondissementCsv = testCsvArrondissementen.data.find(element => element?.Arrondissement?.split?.(' ')[1].toUpperCase() === arrondissement.properties.NAAM.toUpperCase());
-      arrondissement.properties.mediaan1 = ObjectArrondissementCsv.mediaan1;
+      const ObjectArrondissementCsv = activeYears.find(element => element?.Arrondissement?.split?.(' ')[1].toUpperCase() === arrondissement.properties.NAAM.toUpperCase());
       
-      
+      arrondissement.properties.mediaan1 = Number(ObjectArrondissementCsv.mediaan1);
+      console.log(arrondissement.properties.mediaan1);
+      // for(let i = 0; i < stateGeojsonArrondissementenVlaanderen.features)
+      // console.log(arrondissement.properties.mediaan1);
     }
+    
+    setStateGeojsonArrondissementenVlaanderen(geojsonArrondissementenVlaanderen);
+    setKeyData(Math.random());
+    // setGeojsonArrondissementen()
     //{ ...geojsonGemeentesVlaanderen.features[i].properties, ...ObjectMunicipalityCsv } 
     //setGeojson(...geojson.features)
-  }, [])
+  }, [year])
+
+  //useEffect(() => {setStateGeojsonArrondissementenVlaanderen(stateGeojsonArrondissementenVlaanderen)}, [stateGeojsonArrondissementenVlaanderen]);
   const municipality = zoekGemeente('Aarschot');
 
   const onHover = useCallback(event => {
@@ -207,22 +314,19 @@ function App() {
     // console.log(hoveredFeature);
   }, []);
 
-  const data = useMemo(() => {
-    return allData; //&& updatePercentiles(allData, f => f.properties.income[year])
-  }, [allData, year]);
-
+  // const data = useMemo(() => {
+  //   setYear(year); //&& updatePercentiles(allData, f => f.properties.income[year])
+  // }, [allData, year]);
+  //return <> {JSON.stringify(stateGeojsonArrondissementenVlaanderen?.features.map((feature) => feature.properties.mediaan1))} <ControlPanel year={year} onChange={value => setYear(value)} /></>
   return (
 <>
     <Map ref={mapRef}
       interactiveLayerIds={[
-        "mediaan prijs",
         "arrondissementen",
-        "provincies"
       ]
       }
       onLoad={
         () => {
-          setLoaded(true);
 
           //geojsonGemeentesVlaanderen.features[0].properties.mediaan1 = 300001;
           // console.log(geojsonGemeentesVlaanderen);
@@ -240,20 +344,21 @@ function App() {
       mapboxAccessToken={MAPBOX_TOKEN}
       onMouseMove={onHover}
       onMouseLeave={()=>setHoverInfo(null)}
-      onZoom={() => {if(mapRef.current.getZoom() < 8){setDisplayLegend('none')}else{setDisplayLegend('block')}
-    console.log(displayLegend)}}
+      onZoom={() => {if(mapRef.current.getZoom() > 10){setLegendItemsLayer(legendItemsGemeentes)} 
+                    else if(mapRef.current.getZoom() > 8 && mapRef.current.getZoom() < 11){setLegendItemsLayer(legendItemsArrondissementen)} 
+                    else{setLegendItemsLayer(legendItemsProvincies)}}}
       //onZoom={() => {if(mapRef.current.getZoom() > 4){mapRef.current.getLayer('provincies') = 'block'; mapRef.current.getLayer('mediaan prijs').style.display = 'none';} 
       //else{mapRef.current.getLayer('mediaan prijs').style.display = 'block'; mapRef.current.getLayer('provincies').style.display = 'none';}}}
     >
       {hoverInfo && (
         <div className="tooltip">
-          <div><span>{hoverInfo.feature.properties.NAAM}</span></div>
+          <div><span>{hoverInfo.feature.properties.mediaan1}</span></div>
           {/* <div><div>Provincie: <span>{hoverInfo.feature.properties.province}</span></div></div>
           <div><div>Regio: <span>{hoverInfo.feature.properties.region}</span></div></div>
           <div><div>Shn: <span>{hoverInfo.feature.properties.shn}</span></div></div> */}
         </div>
       )}
-      {displayLegend && (<div id="legend" className="legend" style={{display: {displayLegend}}}>
+      {/* {displayLegend && (<div id="legend" className="legend" style={{display: {displayLegend}}}>
         <h4>Mediaan prijs (€)</h4>
         <div><div style={{ backgroundColor: '#3C3A38' }}></div>Niet geweten</div>
         <div><div style={{ backgroundColor: '#A0A0A0' }}></div>Undefined</div>
@@ -266,10 +371,12 @@ function App() {
         <div><div style={{ backgroundColor: '#EED322' }}></div>100000</div>
         <div><div style={{ backgroundColor: '#F2F12D' }}></div>0</div>
 
-      </div>)}
-      
+      </div>)} */}
 
-      <Source id='mediaan prijs' type='geojson' data={geojsonGemeentesVlaanderen}>
+      
+      <Legend legendItems={legendItemsLayer} title={'Mediaan prijs (€)'}/>
+      
+      {/* <Source id='mediaan prijs' type='geojson' data={geojsonGemeentesVlaanderen}>
         <Layer 
           {
           ...{
@@ -322,7 +429,7 @@ function App() {
               'line-width': 2
               }
               
-          }}></Layer>
+          }}></Layer> */}
         {/* <Layer {
           ...{
             'id': 'label',
@@ -373,12 +480,13 @@ function App() {
           > 
         </Layer> */}
 
-      </Source>
-      <Source id='arrondissementen' type='geojson' data={geojsonArrondissementenVlaanderen}>
+      {/* </Source> */}
+      <Source id={'arrondissementen' + keyData} type='geojson' data={stateGeojsonArrondissementenVlaanderen}>
         <Layer {
         ...{
+          'key': 'arrondissementen' + keyData,
           'id': 'arrondissementen',
-          'source': 'arrondissementen',
+          'source': 'arrondissementen' + keyData,
           'minzoom': 8,
           'maxzoom': 10,
           'type': 'fill',
@@ -389,13 +497,13 @@ function App() {
                 ['linear'],
                 ['get', 'mediaan1'],
                 179,
-                '#F2F12D',
+                '#FF0000',
                 183,
-                '#EED322',
+                '#00FF00',
                 184.99,
-                '#E6B71E',
+                '#0000FF',
                 214,
-                '#DA9C20',
+                '#FFFF00',
                 241,
                 '#CA8323',
                 600000,
@@ -416,7 +524,7 @@ function App() {
         {
           ...
             {
-              'id': 'outline2',
+              'id': 'outline2' + keyData,
               'type': 'line',
               'source': 'mediaan prijs',
               'minzoom': 8,
@@ -429,8 +537,8 @@ function App() {
               
           }}></Layer>
       
-        </Source>
-        <Source id='provincies' type='geojson' data={geojsonProvinciesVlaanderen}>
+      </Source>
+      {/* <Source id='provincies' type='geojson' data={geojsonProvinciesVlaanderen}>
         <Layer {
 
         ...{
@@ -482,23 +590,11 @@ function App() {
               }
               
           }}></Layer>     
-        </Source>
-      {/* <Source id='oost' type='geojson' data={testOostVlaanderen}>
-                <Layer
-                {
-                  ...{
-                    'id': 'Oost',
-                    'source': 'oost',
-                    'type': 'fill',
-                    'paint':{
-                      'fill-color': '#A0A0A0'
-                    }
-                    
-                  }
-                }></Layer>
-              </Source> */}
+      </Source> */}
+    </Map>      
 
-    </Map>
+          <button onClick={() => setYear(2015)}>2015</button>
+
     <ControlPanel year={year} onChange={value => setYear(value)} />
     </>
   );
